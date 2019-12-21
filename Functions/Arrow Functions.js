48d16bdf050799935a3406bd2d983bdb500dd084
cielgrisdemoscou <hautcielbleu@gmail.com>
2019-12-21 09:03:14 +0000

    #1 Function

---
+++ Functions/Arrow Functions.js
@@ -0,0 +1,23 @@
+const names = ['ted', 'nano', 'nico'];
+
+/*
+const test = names.map(function(item){
+  return item + " test";
+});
+
+console.log(test);
+*/
+
+/*
+const test = names.map(item => {
+  return item + " test";
+});
+
+const test2 = names.map((item,index) => {
+	console.log("we are on ", index);
+  return item + " test";
+});
+*/
+
+const test = names.map(item => item + " test");
+
